59731d3250dcc74acf8aba8c9603a23389ceba24
Naga sai krishna sujith Nekkanti <sujith@Nagas-MacBook-Air.local>
2025-01-03 18:51:00 -0500

    initial  code for portfolio

---
+++ src/App.js
@@ -0,0 +1,39 @@
+/* import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+
+
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar /> 
+      <Hero /> 
+       <About />
+      <Skills /> 
+      <Projects /> 
+      <Contact /> 
+    </div>
+  );
+}
+
+export default App;
+ */
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Sujith!</h1>
+      <p>This is a test message to check if the app renders.</p>
+    </div>
+  );
+}
+
+export default App;
